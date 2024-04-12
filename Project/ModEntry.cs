4a71de0281b4a6bdd4303f2c249c79be1d7cfec1
Khloe Leclair <xivkhloeleclair@gmail.com>
2024-04-12 15:00:19 -0400

    Initial commit.

---
+++ Project/ModEntry.cs
@@ -0,0 +1,15 @@
+﻿using StardewModdingAPI;
+
+namespace SafeProjectName;
+
+public class ModEntry : Mod {
+
+	public override void Entry(IModHelper helper) {
+		// Initialize the Translation Helper
+		I18n.Init(Helper.Translation);
+
+		// Say Hello.
+		Monitor.Log(I18n.Hello("World"), LogLevel.Info);
+	}
+
+}
